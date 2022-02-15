f93672ac8b8184ef8fd49a388b621380aec9577a
J <eri@Eri-2.local>
2022-02-15 21:28:23 +0900

    Added Output File Writer

--- test/OutputFileWriter.java
+++ test/OutputFileWriter.java
@@ -1,10 +1,20 @@
 import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 public class OutputFileWriter {
 
     public static void writeToOutputFile(Output output, String fileName) {
         try {
             PrintWriter writer = new PrintWriter(fileName, "UTF-8");
+
+            writer.println(output.getNumSlides());
+            for (Output.SlideInformation slide : output.getSlideInformationList()) {
+                writer.println(slide.getPhotoIdList().stream()
+                        .map(String::valueOf)
+                        .collect(Collectors.joining(" "))
+                );
+            }
 
             writer.close();
         } catch (Exception e) {
