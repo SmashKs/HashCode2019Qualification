4a040ad529dc75c4d258b9c101003b68431686ea
J <eri@Eri-2.local>
2022-02-15 21:24:45 +0900

    Added Output class

--- src/Output.java
+++ src/Output.java
@@ -2,4 +2,32 @@ import java.util.List;
 
 public class Output {
 
+    private final int numSlides; // Number of slides in the slideshow
+
+    private final List<SlideInformation> slideInformationList;
+
+    public Output(int numSlides, List<SlideInformation> slideInformationList) {
+        this.numSlides = numSlides;
+        this.slideInformationList = slideInformationList;
+    }
+
+    public int getNumSlides() {
+        return numSlides;
+    }
+
+    public List<SlideInformation> getSlideInformationList() {
+        return slideInformationList;
+    }
+
+    class SlideInformation {
+        private final List<Integer> photoIdList;
+
+        public SlideInformation(List<Integer> photoIdList) {
+            this.photoIdList = photoIdList;
+        }
+
+        public List<Integer> getPhotoIdList() {
+            return photoIdList;
+        }
+    }
 }
