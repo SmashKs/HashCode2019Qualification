0aeeb0b6cbafd56796a7c5cfd3888aee266cb387
jieyi <pokkbaby@gmail.com>
2022-02-17 22:14:11 +0900

    chore(*): add the score of ours and online one

--- test/SolutionTest.java
+++ test/SolutionTest.java
@@ -7,9 +7,9 @@ import java.util.stream.Stream;
 public class SolutionTest {
     public static Stream<Arguments> dataSource() {
         return Stream.of(Arguments.of("input/a.txt", "output/a.txt"),
-                         // Arguments.of("input/b.txt", "output/b.txt"),
+                         Arguments.of("input/b.txt", "output/b.txt"),
                          Arguments.of("input/c.txt", "output/c.txt"),
-                         // Arguments.of("input/d.txt", "output/d.txt"),
+                         Arguments.of("input/d.txt", "output/d.txt"),
                          Arguments.of("input/e.txt", "output/e.txt"));
     }
 
@@ -21,6 +21,16 @@ public class SolutionTest {
         OutputFileWriter.writeToOutputFile(solution.solution(input), outputFilePath);
         Output answer = OutputFileParser.parseOutputFile(outputFilePath);
         long score = ScoreCalculator.calculateScore(input, answer);
+        System.out.println(score);
+    }
+
+    @ParameterizedTest
+    @MethodSource("dataSource")
+    public void testFromOutput(String inputFilePath, String outputFilePath) {
+        Input input = InputFileParser.parseInputFile(inputFilePath);
+        Output answer = OutputFileParser.parseOutputFile(outputFilePath);
+        long score = ScoreCalculator.calculateScore(input, answer);
+
         System.out.println(score);
     }
 }
